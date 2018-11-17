236e8e272d13d8e5687458991568f08a556a6cd4
Paul <pfultz2@yahoo.com>
2018-11-16 19:54:19 -0600

    Add transformation to simplify regex

--- tools/eval.py
+++ tools/eval.py
@@ -1,5 +1,42 @@
 import re, sys
 
+symbols = {
+    '[': '\\[',
+    '[[': '[',
+    ']': '\\]',
+    ']]': ']',
+    '(': '\\(',
+    '((': '(',
+    ')': '\\)',
+    '))': ')',
+    '++': '\\+\\+',
+    '--': '\\-\\-'
+}
+
+def q_word(word):
+    if word in symbols:
+        return symbols[word]
+    elif word.startswith('(') and word != '(':
+        return '('+q_word(word[1:])
+    elif word.endswith(')') and word != ')':
+        return q_word(word[0:-1])+')'
+    elif word.startswith('$'):
+        return '\\'+word[1:]
+    else:
+        return word
+
+def q_words(s):
+    words = s.split()
+    for word in words:
+        if '|' in word and not word.startswith('|'):
+            yield '|'.join([q_word(x) for x in word.split('|')])
+        else:
+            yield q_word(word)
+
+def q(s):
+    return ' '.join(q_words(s))
+
+
 def block():
     return '({[^{}]*(?1)*[^{}]*})'
 
@@ -7,7 +44,8 @@ def paren():
     return "\\( [^()]+ \\)"
 
 def for_idx_loop(inner):
-    return "for \\( \\w+ (\\w+) = \\w+ ; \\1 < \\w+ ; (\\1 \\+\\+|\\+\\+ \\1|\\1 \\-\\-|\\-\\- \\1) \\) {{ {} }}".format(inner)
+    # return "for \\( \\w+ (\\w+) = \\w+ ; \\1 < \\w+ ; (\\1 \\+\\+|\\+\\+ \\1|\\1 \\-\\-|\\-\\- \\1) \\) {{ {} }}".format(inner)
+    return q("for ( $w+ ($w+) = $w+ ; $1 < $w+ ; ($1 ++|++ $1|$1 --|-- $1) ) {{ {} }}").format(inner)
 
 def template_eval(template, **kwargs):
     start = '<%'
