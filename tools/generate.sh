c9c190be7e9c40c29f0a9ebecbb46f6e5d005dec
Paul <pfultz2@yahoo.com>
2018-11-16 19:05:57 -0600

    Fix the script

--- tools/generate.sh
+++ tools/generate.sh
@@ -1,3 +1,3 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-find $DIR | grep "in$" | sed 's/.in$//g' | xargs -n 1 -P 8 -I{} -t bash -c "python $DIR/{}.in > $DIR/{}.xml"
+find $DIR/rules | grep "in$" | sed 's/.in$//g' | xargs -n 1 -P 8 -I{} -t bash -c "python $DIR/tools/eval.py {}.in > {}.xml"
